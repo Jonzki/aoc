101aded517e9e750acc91162775e01c2df21dbd8
jonzki <jonzki1@gmail.com>
2020-12-06 09:51:12 +0200

    Solve 2020.5

---
+++ Aoc/Aoc.Tests/Problems/Aoc20/Problem5Tests.cs
@@ -0,0 +1,28 @@
+﻿using Aoc.Problems.Aoc20;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc20
+{
+    [TestClass]
+    public class Problem5Tests
+    {
+        [DataTestMethod]
+        [DataRow("FBFBBFFRLR", 44, 5, 357)]
+        [DataRow("BFFFBBFRRR", 70, 7, 567)]
+        [DataRow("FFFBBBFRRR", 14, 7, 119)]
+        [DataRow("BBFFBBFRLL", 102, 4, 820)]
+        public void ParseBoardingPass_WorksFor_SmallInput(object input, object correctRow, object correctCol, object correctSeatId)
+        {
+            var pass = Problem5.ParseBoardingPass((string)input);
+
+            Assert.AreEqual(correctRow, pass.Row);
+            Assert.AreEqual(correctCol, pass.Column);
+            Assert.AreEqual(correctSeatId, pass.SeatId);
+        }
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc20/Problem5.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace Aoc.Problems.Aoc20
+{
+    public class Problem5 : IProblem
+    {
+        public object Solve1(string input)
+        {
+            var boardingPasses = input
+                .Split('\n')
+                .Select(x => ParseBoardingPass(x.Trim()))
+                .ToArray();
+
+            // What is the highest seat ID on a boarding pass?
+            return boardingPasses.Max(x => x.SeatId);
+        }
+
+        public object Solve2(string input)
+        {
+            // We're only interested in the seat IDs.
+            var seatIds = input
+                .Split('\n')
+                .Select(x => ParseBoardingPass(x.Trim()).SeatId)
+                .OrderBy(x => x)
+                .ToArray();
+
+            // Look for a seat between two others.
+            for (var i = 0; i < seatIds.Length - 1; ++i)
+            {
+                if (seatIds[i + 1] - seatIds[i] == 2) return seatIds[i] + 1;
+            }
+
+            return -1;
+        }
+
+        public static BoardingPass ParseBoardingPass(string input)
+        {
+            // First 7 chars for row, last 3 for column.
+            var rowInput = input.Substring(0, 7);
+            var colInput = input.Substring(7);
+
+            // Binary search for row.
+            var row = BinarySearch(0, 127, rowInput);
+            var col = BinarySearch(0, 7, colInput);
+
+            return new BoardingPass { Row = row, Column = col };
+        }
+
+        public static int BinarySearch(int start, int end, string input)
+        {
+            // Binary search for row.
+            int blockStart = start, blockEnd = end;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                var half = (1 + blockEnd - blockStart) / 2;
+                if (input[i] == 'B' || input[i] == 'R')
+                {
+                    // Back half.
+                    blockStart += half;
+                }
+                else if (input[i] == 'F' || input[i] == 'L')
+                {
+                    // Front half.
+                    blockEnd -= half;
+                }
+                else throw new InvalidOperationException($"Unexpected char '{input[i]}' at position {i}.");
+            }
+
+            if (blockEnd - blockStart != 0)
+            {
+                throw new InvalidOperationException("Failed to find row.");
+            }
+
+            return blockStart;
+        }
+
+        public record BoardingPass
+        {
+            public int Row { get; set; }
+
+            public int Column { get; set; }
+
+            public int SeatId => 8 * Row + Column;
+        }
+    }
+}
