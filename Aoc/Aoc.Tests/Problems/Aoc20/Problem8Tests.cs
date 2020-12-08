a77833466784b09da2e1dc6587b4c394612dfc4d
jonzki <jonzki1@gmail.com>
2020-12-08 21:01:30 +0200

    Solve 2020.8

---
+++ Aoc/Aoc.Tests/Problems/Aoc20/Problem8Tests.cs
@@ -0,0 +1,27 @@
+﻿using Aoc.Problems.Aoc20;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aoc.Tests.Problems.Aoc20
+{
+    [TestClass]
+    public class Problem8Tests
+    {
+        [DataTestMethod]
+        [DataRow("nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6", 5)]
+        public void Solve1_WorksFor_SmallInput(string input, int correctOutput)
+        {
+            var output = new Problem8().Solve1(input);
+
+            Assert.AreEqual(correctOutput, output);
+        }
+
+        [DataTestMethod]
+        [DataRow("nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6", 8)]
+        public void Solve2_WorksFor_SmallInput(string input, int correctOutput)
+        {
+            var output = new Problem8().Solve2(input);
+
+            Assert.AreEqual(correctOutput, output);
+        }
+    }
+}
---
+++ Aoc/Aoc/Utils/StringUtils.cs
@@ -0,0 +1,7 @@
+﻿namespace Aoc.Utils
+{
+    public static class StringUtils
+    {
+        public static string[] SplitLines(this string input) => input.Replace("\r", "").Split("\n");
+    }
+}
