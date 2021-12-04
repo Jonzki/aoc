26e99f69cb9d60e4ccc73f72adc18c8cec86f585
Joni <joni.saarelainen@delta.fi>
2021-12-04 18:32:24 +0200

    Solve 2021.4

---
+++ Aoc/Aoc.Tests/Problems/Aoc21/Problem4Tests.cs
@@ -0,0 +1,75 @@
+﻿using Aoc.Problems.Aoc21;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc21;
+
+[TestClass]
+public class Problem4Tests
+{
+
+    private const string SmallInput = @"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7";
+
+    [TestMethod]
+    public void ParseInput_Handles_SmallInput()
+    {
+        var (boards, numbers) = Problem4.ParseInput(SmallInput);
+
+        Assert.AreEqual(27, numbers.Count);
+        Assert.AreEqual(3, boards.Count);
+    }
+
+    [DataTestMethod]
+    [DataRow(22, 13, 17, 11, 0)]
+    [DataRow(13, 2, 9, 10, 12)]
+    public void CheckBingo_Checks_Bingo(params int[] numbers)
+    {
+        var (boards, _) = Problem4.ParseInput(SmallInput);
+        var board = boards[0];
+
+        var isBingo = false;
+        foreach (var num in numbers)
+        {
+            isBingo |= board.AddNumber(num);
+        }
+        Assert.IsTrue(isBingo);
+    }
+
+    [DataTestMethod]
+    [DataRow(4512, SmallInput)]
+    public void Part1_SmallInput_Is_Correct(object correctOutput, string input)
+    {
+        var output = new Problem4().Solve1(input);
+        Assert.AreEqual(correctOutput, output);
+    }
+
+    [DataTestMethod]
+    [DataRow(1924, SmallInput)]
+    public void Part2_SmallInput_Is_Correct(object correctOutput, string input)
+    {
+        var output = new Problem4().Solve2(input);
+        Assert.AreEqual(correctOutput, output);
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc21/Problem4.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc21;
+
+public class Problem4 : IProblem
+{
+    public class BingoBoard
+    {
+        public int Id { get; init; }
+        public int[,] Values { get; init; }
+        public bool[,] Checks { get; init; }
+        public bool HasBingo { get; private set; }
+
+        public static BingoBoard Parse(string input, int index)
+        {
+            // Assume a 5x5 board.
+            var values = input.Split(new string[] { Environment.NewLine, " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var boardNumbers = ArrayUtils.To2D(values, 5, 5);
+
+            var board = new BingoBoard
+            {
+                Id = index,
+                Values = boardNumbers,
+                Checks = new bool[5, 5]
+            };
+            return board;
+        }
+
+        /// <summary>
+        /// Adds one number to the board. Returns true if there's a Bingo, false otherwise.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool AddNumber(int number)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    if (Values[x, y] == number)
+                    {
+                        Checks[x, y] = true;
+                    }
+                }
+            }
+            var bingo = CheckForBingo();
+            if (bingo)
+            {
+                HasBingo = true;
+            }
+            return bingo;
+        }
+
+        /// <summary>
+        /// Checks if there's a Bingo (any filled row or column).
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckForBingo()
+        {
+            var rows = new[] { true, true, true, true, true };
+            var cols = new[] { true, true, true, true, true };
+
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    rows[x] &= Checks[x, y];
+                    cols[y] &= Checks[x, y];
+                }
+            }
+
+            return rows.Any(x => x) || cols.Any(y => y);
+        }
+
+        /// <summary>
+        /// Calculates the winning Bingo score for the board.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        internal object CalculateScore(int number)
+        {
+            // Start by finding the sum of all unmarked numbers on that board.
+            var unmarkedSum = 0;
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    if (!Checks[x, y])
+                    {
+                        unmarkedSum += Values[x, y];
+                    }
+                }
+            }
+
+            // Then, multiply that sum by the number that was just called when the board won to get the final score.
+            return unmarkedSum * number;
+        }
+    }
+
+    public object Solve1(string input)
+    {
+        var (boards, numbers) = ParseInput(input);
+
+        foreach (var number in numbers)
+        {
+            foreach (var board in boards)
+            {
+                // Add number, check for bingo.
+                if (board.AddNumber(number))
+                {
+                    Console.WriteLine($"Bingo on board {board.Id}!");
+                    return board.CalculateScore(number);
+                }
+
+            }
+        }
+
+        Console.WriteLine("No winner.");
+        return -1;
+    }
+
+    public object Solve2(string input)
+    {
+        var (boards, numbers) = ParseInput(input);
+        var boardsRemaining = boards.Count;
+
+        foreach (var number in numbers)
+        {
+            foreach (var board in boards)
+            {
+                // Bingoed boards don't need to play anymore.
+                if (board.HasBingo) continue;
+
+                // Add number, check for bingo.
+                var isBingo = board.AddNumber(number);
+                if (isBingo)
+                {
+                    boardsRemaining--;
+                    // "Figure out which board will win last."
+                    if (boardsRemaining == 0)
+                    {
+                        // "Once it wins, what would its final score be?"
+                        return board.CalculateScore(number);
+                    }
+                }
+
+            }
+        }
+
+        Console.WriteLine("No winner.");
+        return -1;
+    }
+
+    public static (List<BingoBoard> Boards, List<int> Numbers) ParseInput(string input)
+    {
+        // Starts by splitting with double line ends.
+        var blocks = input.Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        // First block is the numbers array - split with comma.
+        var numbers = blocks[0].Split(',').Select(int.Parse).ToList();
+
+        // Remaining blocks are bingo boards.
+        var boards = blocks.Skip(1).Select((input, index) => BingoBoard.Parse(input, index)).ToList();
+
+        return (boards, numbers);
+    }
+}
