8e5673797a072afb54b1226482a7a6bae22264b0
Joni <joni.saarelainen@delta.fi>
2021-12-05 10:30:06 +0200

    Solve 2021.5

---
+++ Aoc/Aoc.Tests/Problems/Aoc21/Problem5Tests.cs
@@ -0,0 +1,57 @@
+﻿using Aoc.Problems.Aoc21;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc21;
+
+[TestClass]
+public class Problem5Tests
+{
+
+    private const string SmallInput = @"0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2";
+
+    [DataTestMethod]
+    [DataRow(5, SmallInput)]
+    public void Part1_SmallInput_Is_Correct(object correctOutput, string input)
+    {
+        var output = new Problem5().Solve1(input);
+        Assert.AreEqual(correctOutput, output);
+    }
+
+    [DataTestMethod]
+    [DataRow(12, SmallInput)]
+    public void Part2_SmallInput_Is_Correct(object correctOutput, string input)
+    {
+        var output = new Problem5().Solve2(input);
+        Assert.AreEqual(correctOutput, output);
+    }
+
+    [DataTestMethod]
+    [DataRow(2, "1,1;1,2")]
+    [DataRow(4, "1,1;4,1")]
+    [DataRow(1, "1,1;1,1")]
+    [DataRow(2, "2,2;2,1")]
+    public void LineConstructor_Works_Correctly(int correctPointCount, string input)
+    {
+        var n = input.Split(',', ';').Select(int.Parse).ToArray();
+
+        // Build a line.
+        var line = new Problem5.Line(new Aoc.Utils.Point2D(n[0], n[1]), new Aoc.Utils.Point2D(n[2], n[3]));
+
+        // Check point count.
+        Assert.AreEqual(correctPointCount, line.Points.Count);
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc21/Problem5.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc21;
+
+public class Problem5 : IProblem
+{
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class Line
+    {
+        public Line(Point2D start, Point2D end)
+        {
+            Points = new List<Point2D>();
+
+            // Calculate the direction to move towards the end.
+            var dir = new Point2D(Math.Sign(end.X - start.X), Math.Sign(end.Y - start.Y));
+
+            for (var i = 0; i < 1000; ++i)
+            {
+                Points.Add((start.X + i * dir.X, start.Y + i * dir.Y));
+
+                if (Points.Last() == end) break;
+            }
+        }
+
+        public List<Point2D> Points { get; set; }
+
+        private string DebuggerDisplay => $"{Points.First()} -> {Points.Last()}";
+    }
+
+    public object Solve1(string input)
+    {
+        // Start by parsing all Lines. Skip diagonals.
+        var lines = ParseLines(input, false);
+
+        if (Debugger.IsAttached) DrawLines(lines);
+
+        // "you need to determine the number of points where at least two lines overlap."
+        var points = lines.SelectMany(line => line.Points).ToArray(); // Select every point from all lines.
+        var groups = points.GroupBy(p => p.ToString()).ToDictionary(g => g.Key, g => g.Count()); // Group same points together.  
+        var overlaps = groups.Count(x => x.Value > 1); // Count overlaps (multiples of same point).
+
+        return overlaps;
+    }
+
+    public object Solve2(string input)
+    {
+        // Start by parsing all Lines. Allow diagonals.
+        var lines = ParseLines(input, true);
+
+        if (Debugger.IsAttached) DrawLines(lines);
+
+        // "you need to determine the number of points where at least two lines overlap."
+        var points = lines.SelectMany(line => line.Points).ToArray(); // Select every point from all lines.
+        var groups = points.GroupBy(p => p.ToString()).ToDictionary(g => g.Key, g => g.Count()); // Group same points together.  
+        var overlaps = groups.Count(x => x.Value > 1); // Count overlaps (multiples of same point).
+
+        return overlaps;
+    }
+
+    public static List<Line> ParseLines(string input, bool allowDiagonals)
+    {
+        var lines = new List<Line>();
+        var rows = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (var row in rows)
+        {
+            // Split by separator and comma. Should become 4 items.
+            var parts = row.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var start = Point2D.Parse(parts[0]);
+            var end = Point2D.Parse(parts[1]);
+
+            // Allow skipping diagonals.
+            var isDiagonal = start.X != end.X && start.Y != end.Y;
+            if (isDiagonal && !allowDiagonals) continue;
+
+            lines.Add(new Line(start, end));
+        }
+        return lines;
+    }
+
+    private void DrawLines(List<Line> lines)
+    {
+        int maxX = 0, maxY = 0;
+        var counts = new Dictionary<(int X, int Y), int>();
+        foreach (var line in lines)
+        {
+            foreach (var point in line.Points)
+            {
+                if (!counts.ContainsKey(point)) { counts[point] = 0; };
+                counts[point]++;
+
+                if (point.X > maxX) maxX = point.X;
+                if (point.Y > maxY) maxY = point.Y;
+            }
+        }
+
+        if (maxX > 50 || maxY > 50)
+        {
+            Console.WriteLine("Map is too large to draw.");
+            return;
+        }
+
+        for (var y = 0; y <= maxY; y++)
+        {
+            for (var x = 0; x <= maxX; x++)
+            {
+                if (counts.TryGetValue((x, y), out var count))
+                {
+                    Console.Write(count);
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine("");
+        }
+
+    }
+}
