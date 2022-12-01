6052c812627c48b00366f2a623a1b8e7f360fed6
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-01 08:42:50 +0200

    Solve 2022.1

---
+++ Aoc/Aoc.Tests/Problems/Aoc22/Problem01Tests.cs
@@ -0,0 +1,38 @@
+﻿using Aoc.Problems.Aoc22;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc22;
+
+[TestClass]
+public class Problem01Tests : ProblemTests<Problem01>
+{
+    protected override string SmallInput => @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    protected override object CorrectOutput1 => 24000;
+
+    protected override object CorrectOutput2 => 45000;
+
+    [TestMethod]
+    public void Part1_SmallInput_Is_Correct() => RunPart1(CorrectOutput1, SmallInput);
+
+    [TestMethod]
+    public void Part2_SmallInput_Is_Correct() => RunPart2(CorrectOutput2, SmallInput);
+}
---
+++ Aoc/Aoc/Problems/Aoc22/Problem01.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc22;
+
+/// <summary>
+/// https://adventofcode.com/2022/day/1
+/// </summary>
+public class Problem01 : IProblem
+{
+    internal struct Elf
+    {
+        public List<int> Foods { get; set; }
+
+        /// <summary>
+        /// Returns the total amount of calories in the Foods.
+        /// </summary>
+        public int TotalCalories => Foods.Sum();
+    }
+
+    public object Solve1(string input)
+    {
+        // Parse the elves, return the largest calorie total.
+        var largestCalories = ParseElves(input)
+            .OrderByDescending(e => e.TotalCalories)
+            .FirstOrDefault()
+            .TotalCalories;
+
+        return largestCalories;
+    }
+
+    public object Solve2(string input)
+    {
+        // For part 2, instead of the top calorie count, we need top 3.
+        var calories = ParseElves(input)
+            .OrderByDescending(e => e.TotalCalories)
+            .Take(3)
+            .Sum(e => e.TotalCalories);
+
+        return calories;
+    }
+
+    /// <summary>
+    /// Parses the input string into a list of elves (sets of calories).
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    internal static List<Elf> ParseElves(string input)
+    {
+        var elves = new List<Elf>();
+
+        // Split the input with double-endline first to get the elves.
+        var parts = input.Split(Environment.NewLine + Environment.NewLine);
+
+        foreach (var part in parts)
+        {
+            // Split each part with a single newline to get the food values.
+            var elf = new Elf
+            {
+                Foods = InputReader.ParseNumberList(part, Environment.NewLine).ToList()
+            };
+            elves.Add(elf);
+        }
+
+        return elves;
+    }
+
+}
