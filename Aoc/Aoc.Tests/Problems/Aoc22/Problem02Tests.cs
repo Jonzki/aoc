6b40c731a594b60659882795952729ee734826e9
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-02 12:30:56 +0200

    Solve 2022.2

---
+++ Aoc/Aoc.Tests/Problems/Aoc22/Problem02Tests.cs
@@ -0,0 +1,27 @@
+﻿using Aoc.Problems.Aoc22;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc22;
+
+[TestClass]
+public class Problem02Tests : ProblemTests<Problem02>
+{
+    protected override string SmallInput => @"A Y
+B X
+C Z";
+
+    protected override object CorrectOutput1 => 15;
+
+    protected override object CorrectOutput2 => 12;
+
+    [TestMethod]
+    public void Part1_SmallInput_Is_Correct() => RunPart1(CorrectOutput1, SmallInput);
+
+    [TestMethod]
+    public void Part2_SmallInput_Is_Correct() => RunPart2(CorrectOutput2, SmallInput);
+}
---
+++ Aoc/Aoc/Problems/Aoc22/Problem02.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc22;
+
+/// <summary>
+/// https://adventofcode.com/2022/day/2
+/// </summary>
+public class Problem02 : IProblem
+{
+    public object Solve1(string input)
+    {
+        // Split the input into lines.
+        var lines = input.SplitLines();
+
+        // We get points based on whether we win, lose or draw,
+        // and based on which shape we chose.
+        // Since there are only 3*3=9 possible pairs, we can do a simple map.
+
+        var totalScore = 0;
+
+        // Define the scoring.
+        const int
+            win = 6, draw = 3, loss = 0,
+            rock = 1, paper = 2, scissors = 3;
+
+        foreach (var line in lines)
+        {
+            // Win +6, Draw +3, Loss +0
+            // Rock +1, Paper +2, Scissors +3
+            totalScore += line switch
+            {
+                "A X" => draw + rock,       // Rock-rock - DRAW
+                "A Y" => win + paper,       // Rock-paper
+                "A Z" => loss + scissors,   // Rock-scissors
+                "B X" => loss + rock,       // Paper-rock
+                "B Y" => draw + paper,      // paper-paper
+                "B Z" => win + scissors,    // paper-scissors
+                "C X" => win + rock,        // scissors-rock
+                "C Y" => loss + paper,      // scissors-paper
+                "C Z" => draw + scissors,   // scissors-scissors
+                _ => throw new InvalidOperationException($"Unexpected line: '{line}'")
+            };
+        }
+
+        return totalScore;
+    }
+
+    public object Solve2(string input)
+    {
+        // Split the input into lines.
+        var lines = input.SplitLines();
+
+        // We get points based on whether we win, lose or draw,
+        // and based on which shape we chose.
+        // Since there are only 3*3=9 possible pairs, we can do a simple map.
+
+        var totalScore = 0;
+
+        // Define the scoring.
+        const int
+            win = 6, draw = 3, lose = 0,
+            rock = 1, paper = 2, scissors = 3;
+
+        foreach (var line in lines)
+        {
+            // Win +6, Draw +3, Loss +0
+            // Rock +1, Paper +2, Scissors +3
+
+            // For part 2, X needs to lose, Y needs to draw, Z needs to win.
+            // The same 9 options still remain.
+            totalScore += line switch
+            {
+                "A X" => lose + scissors,   // Lose to rock with scissors.
+                "A Y" => draw + rock,       // Draw with rock.
+                "A Z" => win + paper,       // Win rock with paper.
+                "B X" => lose + rock,       // Lose to paper with rock.
+                "B Y" => draw + paper,      // Draw paper with paper
+                "B Z" => win + scissors,    // Win paper with scissors
+                "C X" => lose + paper,      // Lose with paper.
+                "C Y" => draw + scissors,   // Draw with scissors
+                "C Z" => win + rock,        // Win scissors with rock.
+                _ => throw new InvalidOperationException($"Unexpected line: '{line}'")
+            };
+        }
+
+        return totalScore;
+    }
+
+
+}
