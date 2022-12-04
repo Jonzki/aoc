710cdc426a71d384907feda1aeecd9606f611bea
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-04 10:32:45 +0200

    Solve 2022.4

---
+++ Aoc/Aoc.Tests/Problems/Aoc22/Problem04Tests.cs
@@ -0,0 +1,30 @@
+﻿using Aoc.Problems.Aoc22;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc22;
+
+[TestClass]
+public class Problem04Tests : ProblemTests<Problem04>
+{
+    protected override string SmallInput => @"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    protected override object CorrectOutput1 => 2;
+
+    protected override object CorrectOutput2 => 4;
+
+    [TestMethod]
+    public void Part1_SmallInput_Is_Correct() => RunPart1(CorrectOutput1, SmallInput);
+
+    [TestMethod]
+    public void Part2_SmallInput_Is_Correct() => RunPart2(CorrectOutput2, SmallInput);
+}
