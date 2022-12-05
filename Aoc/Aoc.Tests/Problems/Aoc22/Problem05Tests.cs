3678e274c3a1b6f2234611f2dcf6232bddf3ae8c
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-05 08:49:02 +0200

    Solve 2022.5

---
+++ Aoc/Aoc.Tests/Problems/Aoc22/Problem05Tests.cs
@@ -0,0 +1,33 @@
+﻿using Aoc.Problems.Aoc22;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc.Tests.Problems.Aoc22;
+
+[TestClass]
+public class Problem05Tests : ProblemTests<Problem05>
+{
+    protected override string SmallInput => @"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
+
+    protected override object CorrectOutput1 => "CMZ";
+
+    protected override object CorrectOutput2 => "MCD";
+
+    [TestMethod]
+    public void Part1_SmallInput_Is_Correct() => RunPart1(CorrectOutput1, SmallInput);
+
+    [TestMethod]
+    public void Part2_SmallInput_Is_Correct() => RunPart2(CorrectOutput2, SmallInput);
+}
--- Aoc/Aoc/Utils/StringUtils.cs
+++ Aoc/Aoc/Utils/StringUtils.cs
@@ -3,5 +3,29 @@
     public static class StringUtils
     {
         public static string[] SplitLines(this string input) => input.Replace("\r", "").Split("\n");
+
+        /// <summary>
+        /// Removes all occurrences of the input text from the string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="textToRemove"></param>
+        /// <returns></returns>
+        public static string RemoveString(this string input, string textToRemove) => input.Replace(textToRemove, string.Empty);
+
+        /// <summary>
+        /// Removes all occurrences of input texts from the string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="textToRemove"></param>
+        /// <returns></returns>
+        public static string RemoveStrings(this string input, params string[] textsToRemove)
+        {
+            string output = input;
+            foreach (var t in textsToRemove)
+            {
+                output = RemoveString(output, t);
+            }
+            return output;
+        }
     }
 }
