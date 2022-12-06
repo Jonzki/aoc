ffd6be5f0f8714e88a3ec2fb894d74bf630d12a5
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-06 08:10:17 +0200

    Solve 2022.6 (#11)

---
+++ Aoc/Aoc/Problems/Aoc22/Problem06.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc22;
+
+public class Problem06 : IProblem
+{
+    public object Solve1(string input) => FindMarker(input, 4);
+
+    public object Solve2(string input) => FindMarker(input, 14);
+
+    /// <summary>
+    /// Finds a marker position (first substring with distinct characters) in the input.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="markerLength"></param>
+    /// <returns></returns>
+    private static int? FindMarker(string input, int markerLength)
+    {
+        // Find a marker - a string where all characters are unique.
+        for (var i = 0; i <= input.Length - markerLength; ++i)
+        {
+            var block = input.Substring(i, markerLength);
+            var isMarker = block.Distinct().Count() == markerLength;
+
+            // Return the end of the current position.
+            if (isMarker) return i + markerLength;
+        }
+        return null;
+    }
+}
