142cea139525a042b8fa4ea399b6d134b4443145
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2022-12-12 18:30:18 +0200

    Solve 2022.10 part 1

---
+++ Aoc/Aoc.Tests/Problems/Aoc22/Problem10Tests.cs
@@ -0,0 +1,165 @@
+﻿using Aoc.Problems.Aoc22;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aoc.Tests.Problems.Aoc22;
+
+[TestClass]
+public class Problem10Tests : ProblemTests<Problem10>
+{
+    const string SmallInput = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+
+    const int CorrectOutput1 = 13140;
+
+    const int CorrectOutput2 = 1;
+
+    [TestMethod]
+    public void Part1_SmallInput_Is_Correct() => RunPart1(CorrectOutput1, SmallInput);
+
+    [TestMethod]
+    public void Part2_SmallInput_Is_Correct() => RunPart2(CorrectOutput2, SmallInput);
+}
---
+++ Aoc/Aoc/Problems/Aoc22/Problem10.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aoc.Utils;
+
+namespace Aoc.Problems.Aoc22;
+
+/// <summary>
+/// https://adventofcode.com/2022/day/10
+/// </summary>
+public class Problem10 : IProblem
+{
+    public object Solve1(string input)
+    {
+        // Parse commands.
+        var commands = ParseCommands(input);
+
+        // Set up checkpoints.
+        var checkpoints = new Dictionary<int, int>
+        {
+            [20] = 0,
+            [60] = 0,
+            [100] = 0,
+            [140] = 0,
+            [180] = 0,
+            [220] = 0,
+        };
+
+        // Set up a computer.
+        var computer = new Computer(checkpoints);
+
+        computer.Execute(commands);
+
+        return checkpoints.Select(kvp => kvp.Key * kvp.Value).Sum();
+    }
+
+    public object Solve2(string input)
+    {
+        return 0;
+    }
+
+    private Queue<Command> ParseCommands(string input)
+    {
+        var commands = new Queue<Command>();
+        foreach (var line in input.SplitLines())
+        {
+            var parts = line.Split(' ');
+            var command = parts[0] switch
+            {
+                "noop" => new Command { Identifier = "noop", Duration = 1, Value = 0 },
+                "addx" => new Command { Identifier = "addx", Duration = 2, Value = int.Parse(parts[1]) },
+                _ => throw new ArgumentException($"Command '{parts[0]}' is not recognized.")
+            };
+            commands.Enqueue(command);
+        }
+        return commands;
+    }
+
+    class Computer
+    {
+        public Computer(Dictionary<int, int> checkpoints)
+        {
+            Cycle = 0;
+            // The CPU has a single register, X, which starts with the value 1.
+            RegisterX = 1;
+            RegisterXCheckpoints = checkpoints;
+        }
+
+        public int Cycle = 0;
+
+        public int RegisterX { get; set; }
+
+        /// <summary>
+        /// Checkpoints for register X.
+        /// </summary>
+        public Dictionary<int, int> RegisterXCheckpoints { get; set; }
+
+        /// <summary>
+        /// Executes a command.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="value"></param>
+        public void Execute(Queue<Command> commands)
+        {
+            while (commands.TryDequeue(out var command))
+            {
+                // Run through the cycles.
+                for (var i = 1; i <= command.Duration; ++i)
+                {
+                    Cycle++;
+                    if (RegisterXCheckpoints.ContainsKey(Cycle))
+                    {
+                        RegisterXCheckpoints[Cycle] = RegisterX;
+                    }
+
+                    if (i == command.Duration && command.Identifier == "addx")
+                    {
+                        RegisterX += command.Value;
+                    }
+                }
+            }
+        }
+    }
+
+    class Command
+    {
+        public string Identifier { get; set; }
+        public int Value { get; set; }
+        public int Duration { get; set; }
+    }
+
+}
