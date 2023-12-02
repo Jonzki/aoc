3c134d3c98b78aaaee7e6f912eb1d88b2f271ef4
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2023-12-02 09:51:38 +0200

    Solve 2023.02

---
+++ Aoc/Aoc.Tests/Problems/Aoc23/Problem02Tests.cs
@@ -0,0 +1,51 @@
+﻿using Aoc.Problems.Aoc23;
+
+namespace Aoc.Tests.Problems.Aoc23;
+
+[TestClass]
+public class Problem02Tests : ProblemTests<Problem02>
+{
+    private const string SmallInput = @"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    [TestMethod]
+    public void Solve1Test()
+    {
+        RunPart1(8, SmallInput);
+    }
+
+    [TestMethod]
+    public void Solve2Test()
+    {
+        RunPart2(2286, SmallInput);
+    }
+
+    [TestMethod]
+    public void Game_ParsesCorrectly()
+    {
+        var input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        var game = Problem02.Game.Parse(input);
+
+        Assert.IsNotNull(game);
+        Assert.AreEqual(1, game.Id);
+
+        Assert.AreEqual(3, game.Hints.Count);
+
+        Assert.AreEqual((4, 0, 3), game.Hints[0]);
+        Assert.AreEqual((1, 2, 6), game.Hints[1]);
+        Assert.AreEqual((0, 2, 0), game.Hints[2]);
+    }
+
+    [TestMethod]
+    public void LowestPossible_CalculatesCorrectly()
+    {
+        var input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        var game = Problem02.Game.Parse(input);
+
+        Assert.IsNotNull(game);
+        Assert.AreEqual((4, 2, 6), game.LowestPossible());
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc23/Problem02.cs
@@ -0,0 +1,91 @@
+﻿namespace Aoc.Problems.Aoc23;
+
+public class Problem02 : IProblem
+{
+    public object Solve1(string input)
+    {
+        int red = 12, green = 13, blue = 14;
+
+        var output = input
+            .SplitLines()
+            .Select(Game.Parse)
+            .Where(g => g.IsPossible(red, green, blue))
+            .Select(g => g.Id)
+            .Sum();
+
+        return output;
+    }
+
+    public object Solve2(string input)
+    {
+        var output = input
+            .SplitLines()
+            .Select(Game.Parse)
+            .Select(g => g.LowestPossible())
+            .Select(x => x.Red * x.Green * x.Blue)
+            .Sum();
+        return output;
+    }
+
+    internal class Game
+    {
+        public static Game Parse(string input)
+        {
+            var parts = input.Split(':');
+
+            var id = int.Parse(parts[0].Substring("Game ".Length));
+
+            var hints = new List<(int Red, int Green, int Blue)>();
+
+            parts = parts[1].Split(';');
+            foreach (var hintString in parts)
+            {
+                var colors = hintString.Split(',', ' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int.TryParse(colors.FirstOrDefault(c => c.EndsWith("red"))?.Replace("red", ""), out var red);
+                int.TryParse(colors.FirstOrDefault(c => c.EndsWith("green"))?.Replace("green", ""), out var green);
+                int.TryParse(colors.FirstOrDefault(c => c.EndsWith("blue"))?.Replace("blue", ""), out var blue);
+
+                hints.Add((red, green, blue));
+            }
+
+            return new Game { Id = id, Hints = hints };
+        }
+
+        public int Id { get; init; }
+
+        public List<(int Red, int Green, int Blue)> Hints { get; init; }
+
+        /// <summary>
+        /// Checks if the game is possible for the input amount of cubes.
+        /// </summary>
+        /// <param name="red"></param>
+        /// <param name="green"></param>
+        /// <param name="blue"></param>
+        /// <returns></returns>
+        public bool IsPossible(int red, int green, int blue)
+        {
+            foreach (var hint in Hints)
+            {
+                if (hint.Red > red || hint.Green > green || hint.Blue > blue) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Calculates the lowest possible amount of cubes to make the game possible.
+        /// </summary>
+        /// <returns></returns>
+        public (int Red, int Green, int Blue) LowestPossible()
+        {
+            int r = 0, g = 0, b = 0;
+            foreach (var hint in Hints)
+            {
+                if (hint.Red > r) r = hint.Red;
+                if (hint.Green > g) g = hint.Green;
+                if (hint.Blue > b) b = hint.Blue;
+            }
+            return (r, g, b);
+        }
+    }
+}
