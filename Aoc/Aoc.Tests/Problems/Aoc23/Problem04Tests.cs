752d608b313ed50f0e751714633b697baade33ee
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2023-12-04 19:00:46 +0200

    Solve 2023.04

---
+++ Aoc/Aoc.Tests/Problems/Aoc23/Problem04Tests.cs
@@ -0,0 +1,40 @@
+﻿using Aoc.Problems.Aoc23;
+
+namespace Aoc.Tests.Problems.Aoc23;
+
+[TestClass]
+public class Problem04Tests : ProblemTests<Problem04>
+{
+    private const string SmallInput = @"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    [TestMethod]
+    public void Part1_WorksWith_SmallInput()
+    {
+        RunPart1(13, SmallInput);
+    }
+
+    [TestMethod]
+    public void Part2_WorksWith_SmallInput()
+    {
+        RunPart2(30, SmallInput);
+    }
+
+    [TestMethod]
+    public void CardParsesAndCalculatesCorrectly()
+    {
+        var card = Problem04.Card.Parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+
+        Assert.IsNotNull(card);
+        Assert.AreEqual(1, card.Id);
+        Assert.AreEqual(5, card.WinningNumbers.Length);
+        Assert.AreEqual(8, card.Numbers.Length);
+
+        var value = card.CalculateValue1();
+        Assert.AreEqual(8, value);
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc23/Problem04.cs
@@ -0,0 +1,101 @@
+﻿using System.Buffers;
+
+namespace Aoc.Problems.Aoc23;
+
+public class Problem04 : IProblem
+{
+    public object Solve1(string input)
+    {
+        return input
+            .SplitLines()
+            .Select(Card.Parse)
+            .Sum(c => c.CalculateValue1());
+    }
+
+    public object Solve2(string input)
+    {
+        // Organize the cards into a queue.
+        var cards = new Queue<Card>(
+            input.SplitLines().Select(Card.Parse)
+        ).ToDictionary(c => c.Id);
+
+        var totalCards = cards.Count;
+
+        var cardQueue = new Queue<byte>(cards.Keys);
+
+        while (cardQueue.TryDequeue(out var id))
+        {
+            // Get the winning cards for the current card.
+            var winningCards = cards[id].GetWinningCards();
+
+            // Increase total amount of cards we have.
+            totalCards += winningCards.Length;
+
+            // Add all our cards to queue.
+            foreach (var winningCard in winningCards)
+            {
+                cardQueue.Enqueue(winningCard);
+            }
+        }
+
+        return totalCards;
+    }
+
+    public class Card
+    {
+        private readonly SearchValues<byte> _winSearch;
+
+        public byte Id { get; }
+        public byte[] Numbers { get; }
+        public byte[] WinningNumbers { get; }
+
+        // Buffer for the winning cards (part 2).
+        private byte[]? _winningCards = null;
+
+        /// <summary>
+        /// Parses a Card from the input.
+        /// </summary>
+        /// <param name="input"></param>
+        public Card(string input)
+        {
+            var parts = input.Split(':', '|');
+            Id = byte.Parse(parts[0].Substring(parts[0].IndexOf(' ')));
+            WinningNumbers = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(byte.Parse).ToArray();
+            Numbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(byte.Parse).ToArray();
+            _winSearch = SearchValues.Create(WinningNumbers);
+        }
+
+        public static Card Parse(string input) => new Card(input);
+
+        /// <summary>
+        /// Calculates the value/worth of the card (part 1).
+        /// </summary>
+        /// <returns></returns>
+        public int CalculateValue1()
+        {
+            // Count the amount of numbers.
+            var count = Numbers.Count(_winSearch.Contains);
+            // Total value is 2^(n-1).
+            return (int)Math.Pow(2, count - 1);
+        }
+
+        /// <summary>
+        /// Part 2: returns the output (winning) card numbers.
+        /// </summary>
+        /// <returns></returns>
+        public byte[] GetWinningCards()
+        {
+            if (_winningCards != null)
+            {
+                return _winningCards;
+            }
+            // "you win copies of the scratchcards below the winning card equal to the number of matches"
+            _winningCards = new byte[Numbers.Count(_winSearch.Contains)];
+            for (byte i = 0; i < _winningCards.Length; i++)
+            {
+                _winningCards[i] = (byte)(Id + 1 + i);
+            }
+            return _winningCards;
+        }
+    }
+}
