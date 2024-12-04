6da4d4179f0c6d4c3630bd7337bd5c6741837b3d
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2024-12-04 19:20:17 +0200

    Solve 2024.3

---
+++ Aoc/Aoc.Tests/Problems/Aoc24/Problem03Tests.cs
@@ -0,0 +1,24 @@
+﻿using Aoc.Problems.Aoc24;
+
+namespace Aoc.Tests.Problems.Aoc24
+{
+    [TestClass]
+    public class Problem03Tests : ProblemTests<Problem03>
+    {
+        private const string SmallInput1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+        private const string SmallInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        [TestMethod]
+        public void SolvePart1()
+        {
+            RunPart1(161, SmallInput1);
+        }
+
+        [TestMethod]
+        public void SolvePart2()
+        {
+            RunPart2(48, SmallInput2);
+        }
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc24/Problem03.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Aoc.Problems.Aoc24;
+
+/// <summary>
+/// https://adventofcode.com/2024/day/3
+/// </summary>
+public class Problem03 : IProblem
+{
+    // In (at least) part 1 we are looking for a precise mul(X,Y) instruction.
+    // The lazy (easy) way here is just Regex:
+    public static Regex MulPattern = new Regex(@"(mul\(\d+,\d+\))", RegexOptions.Compiled);
+
+    // In part 2 we also process the do() / don't() commands.
+    // Extend the pattern.
+    public static Regex MulDoDontPattern = new Regex(@"(do\(\))|(don't\(\))|(mul\(\d+,\d+\))", RegexOptions.Compiled);
+
+    public object Solve1(string input)
+    {
+        var matches = MulPattern.Matches(input);
+
+        int sum = 0;
+
+        foreach (Match match in matches)
+        {
+            sum += CalculateMul(match);
+        }
+
+        return sum;
+    }
+
+    public object Solve2(string input)
+    {
+        // Run a similar Regex match, but this time include the control commands.
+        // Regex.Match() returns the matches in the order they occur.
+        var matches = MulDoDontPattern.Matches(input);
+
+        // Track the enabled state:
+        bool enabled = true;
+        int sum = 0;
+
+        foreach (Match match in matches)
+        {
+            // Add in the do/don't command handling
+            if (match.Value == "do()")
+            {
+                enabled = true;
+            }
+            else if (match.Value == "don't()")
+            {
+                enabled = false;
+            }
+            else
+            {
+                // Otherwise add to the sum like before.
+                if (enabled)
+                {
+                    sum += CalculateMul(match);
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    /// Parses a mul(A,B) match and returns the multiplication result.
+    /// </summary>
+    /// <param name="match"></param>
+    /// <returns></returns>
+    private static int CalculateMul(Match match)
+    {
+        // Simple parsing from here on:
+        var numbers = match
+            // Substring: skip 4 first chars "mul(" and the last char ")"
+            .Value[4..^1]
+            // Then split with comma
+            .Split(',')
+            // And parse numbers.
+            .Select(int.Parse)
+            .ToArray();
+
+        // Then simply multiply.
+        return numbers[0] * numbers[1];
+    }
+}
