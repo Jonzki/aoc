f7d387c66297babb09eb0bd6ebae2821200a0599
Joni Saarelainen <joni.saarelainen@kamux.fi>
2024-12-08 13:18:37 +0200

    Start on 2024.08

---
+++ Aoc/Aoc.Tests/Problems/Aoc24/Problem08Tests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Aoc.Problems.Aoc24;
+
+namespace Aoc.Tests.Problems.Aoc24;
+
+[TestClass]
+public class Problem08Tests : ProblemTests<Problem08>
+{
+    public const string SmallInput = """
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
+        """;
+
+    [TestMethod]
+    public void SolvePart1()
+    {
+        RunPart1(14, SmallInput);
+    }
+
+    [TestMethod]
+    public void SolvePart2()
+    {
+        RunPart2(11387L, SmallInput);
+    }
+
+    [TestMethod]
+    public void BuildPairs_Builds_Pairs()
+    {
+        var mapPoints = new List<Problem08.MapPoint>()
+        {
+            // 2 "a" items form a pair 1-2
+            new() { Char = 'a', Id = 1, Position = (1,1)},
+            new() { Char = 'a', Id = 2, Position = (2,2)},
+            // 3 "b" items form 3 possible pairs: 3-4, 3-5 and 4-5
+            new() { Char = 'b', Id = 3, Position = (3,3)},
+            new() { Char = 'b', Id = 4, Position = (4,4)},
+            new() { Char = 'b', Id = 5, Position = (5,5)},
+        };
+
+        var pairs = Problem08.BuildPairs(mapPoints);
+
+        // Should get 4 pairs (1 for a, 3 for b).
+        pairs.Should().HaveCount(1 + 3);
+
+        // "a" pair
+        pairs.Should().Contain(p => Pair(p, 1, 2));
+
+        // "b" pairs
+        pairs.Should()
+            .Contain(p => Pair(p, 3, 4))
+            .And.Contain(p => Pair(p, 3, 5))
+            .And.Contain(p => Pair(p, 4, 5));
+    }
+
+    private bool Pair((int A, int B) pair, int a, int b)
+    {
+        return pair.A == a && pair.B == b
+            || pair.B == a && pair.A == b;
+    }
+
+}
---
+++ Aoc/Aoc/Problems/Aoc24/Problem08.cs
@@ -0,0 +1,135 @@
+﻿namespace Aoc.Problems.Aoc24;
+
+/// <summary>
+/// https://adventofcode.com/2024/day/8
+/// </summary>
+public class Problem08 : IProblem
+{
+    public object Solve1(string input)
+    {
+        var map = ParseMap(input);
+
+        // Calculate all pairs.
+        var pairs = BuildPairs(map.Points);
+
+        var pointLookup = map.Points.ToDictionary(p => p.Id);
+
+        var signalLocations = new HashSet<Point2D>();
+
+        foreach (var pair in pairs)
+        {
+            var pointA = pointLookup[pair.A];
+            var pointB = pointLookup[pair.B];
+
+            var posA = pointA.Position;
+            var posB = pointB.Position;
+
+            // Calculate signal positions as vectors between A and B.
+            // Two directions per pair.
+            signalLocations.Add(posB + (posB - posA));
+            signalLocations.Add(posA + (posA - posB));
+        }
+
+        Draw(map.Points, map.Width, map.Height, signalLocations);
+
+        return signalLocations.Count(p => p.IsInBounds(map.Width, map.Height));
+    }
+
+    public object Solve2(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private (int Width, int Height, List<MapPoint> Points) ParseMap(string input)
+    {
+        var lines = input.SplitLines();
+        var height = lines.Length;
+        var width = lines[0].Length;
+
+        var mapPoints = new List<MapPoint>();
+
+        // Start ID from 1 to leave room for default(int).
+        int id = 1;
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                if (lines[y][x] != '.')
+                {
+                    mapPoints.Add(new MapPoint()
+                    {
+                        Id = id++,
+                        Char = lines[y][x],
+                        Position = new Point2D(x, y)
+                    });
+                }
+            }
+        }
+
+        return (width, height, mapPoints);
+    }
+
+    public struct MapPoint
+    {
+        public int Id { get; init; }
+
+        public char Char { get; init; }
+
+        public Point2D Position { get; init; }
+    }
+
+    private void Draw(List<MapPoint> points, int width, int height, HashSet<Point2D> signalLocations)
+    {
+        for (var y = 0; y < height; y++)
+        {
+            for (var x = 0; x < width; x++)
+            {
+                var point = points.FirstOrDefault(p => p.Position.Equals(x, y));
+                if (point.Id > 0)
+                {
+                    Console.Write(point.Char);
+                }
+                else if (signalLocations.Contains(new Point2D(x, y)))
+                {
+                    Console.Write('#');
+                }
+                else
+                {
+                    Console.Write('.');
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    /// <summary>
+    /// Builds all possible pairs from the input MapPoints.
+    /// </summary>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    public static List<(int A, int B)> BuildPairs(List<MapPoint> points)
+    {
+        var pairs = new List<(int A, int B)>();
+
+        // Process each group separately.
+        foreach (var group in points.GroupBy(p => p.Char))
+        {
+            var count = group.Count();
+            var groupPoints = group.ToList();
+            for (var i = 0; i < count; ++i)
+            {
+                for (var j = 0; j < i; ++j)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+                    pairs.Add((groupPoints[i].Id, groupPoints[j].Id));
+                }
+            }
+        }
+
+        return pairs;
+    }
+
+}
