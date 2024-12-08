e06dc35ada6f52d0470eab0e28e85ccd5564d2e2
Joni Saarelainen <joni.saarelainen@kamux.fi>
2024-12-08 11:21:47 +0200

    Solve 2024.07

---
+++ Aoc/Aoc.Tests/Problems/Aoc24/Problem07Tests.cs
@@ -0,0 +1,88 @@
+﻿using Aoc.Problems.Aoc24;
+using System.Collections.Generic;
+
+namespace Aoc.Tests.Problems.Aoc24
+{
+    [TestClass]
+    public class Problem07Tests : ProblemTests<Problem07>
+    {
+        private const string SmallInput = """
+                                          190: 10 19
+                                          3267: 81 40 27
+                                          83: 17 5
+                                          156: 15 6
+                                          7290: 6 8 6 15
+                                          161011: 16 10 13
+                                          192: 17 8 14
+                                          21037: 9 7 18 13
+                                          292: 11 6 16 20
+                                          """;
+
+        [TestMethod]
+        public void SolvePart1()
+        {
+            RunPart1(3749L, SmallInput);
+        }
+
+        [TestMethod]
+        public void SolvePart2()
+        {
+            RunPart2(11387L, SmallInput);
+        }
+
+        [TestMethod]
+        public void OperationPermutations_WorksCorrectly_ForOne()
+        {
+            // Length of 1 should work:
+            var permutations = Problem07.OperationPermutations(1, new List<char>());
+
+            permutations.Should().HaveCount(2);
+            permutations.Should().AllSatisfy(p => p.Should().HaveCount(1));
+
+            permutations.Should().Contain(p => p[0] == '*');
+            permutations.Should().Contain(p => p[0] == '+');
+        }
+
+        [TestMethod]
+        public void OperationPermutations_WorksCorrectly_ForTwo()
+        {
+            // Length of 1 should work:
+            var permutations = Problem07.OperationPermutations(2, new List<char>());
+
+            permutations.Should().HaveCount(4);
+            permutations.Should().AllSatisfy(p => p.Should().HaveCount(2));
+
+            permutations.Should().Contain(p => new string(p) == "**");
+            permutations.Should().Contain(p => new string(p) == "++");
+            permutations.Should().Contain(p => new string(p) == "*+");
+            permutations.Should().Contain(p => new string(p) == "+*");
+        }
+
+        [DataTestMethod]
+        [DataRow("190: 10 19", 1)]
+        [DataRow("3267: 81 40 27", 2)]
+        [DataRow("83: 17 5", 0)]
+        [DataRow("156: 15 6", 0)]
+        [DataRow("7290: 6 8 6 15", 0)]
+        [DataRow("161011: 16 10 13", 0)]
+        [DataRow("192: 17 8 14", 0)]
+        [DataRow("21037: 9 7 18 13", 0)]
+        [DataRow("292: 11 6 16 20", 1)]
+        public void ProcessEquation_WorksFor_SmallInputLines(string input, int correctPossibleCount)
+        {
+            var (_, possibleCount) = Problem07.ProcessEquation(input);
+            possibleCount.Should().Be(correctPossibleCount);
+        }
+
+        [DataTestMethod]
+        [DataRow("156: 15 6", 1)]
+        [DataRow("7290: 6 8 6 15", 1)]
+        [DataRow("192: 17 8 14", 1)]
+        public void ProcessEquation_WorksFor_Concat(string input, int correctPossibleCount)
+        {
+            var (_, possibleCount) = Problem07.ProcessEquation(input, true);
+            possibleCount.Should().Be(correctPossibleCount);
+        }
+
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc24/Problem07.cs
@@ -0,0 +1,134 @@
+﻿namespace Aoc.Problems.Aoc24;
+
+public class Problem07 : IProblem
+{
+    public object Solve1(string input)
+    {
+        long sum = 0;
+
+        foreach (var line in input.SplitLines())
+        {
+            var output = ProcessEquation(line);
+            if (output.PossibleCount > 0)
+            {
+                sum += output.Result;
+            }
+        }
+
+        return sum;
+    }
+
+    public object Solve2(string input)
+    {
+        long sum = 0;
+
+        List<long> results = new();
+
+        long previous = 0;
+
+        foreach (var line in input.SplitLines())
+        {
+            previous = sum;
+
+            var output = ProcessEquation(line, true);
+            if (output.PossibleCount > 0)
+            {
+                results.Add(output.Result);
+                sum += output.Result;
+
+                if (sum < previous)
+                {
+                    throw new InvalidOperationException("Oops, overflow.");
+                }
+            }
+        }
+        
+        return sum;
+    }
+
+    /// <summary>
+    /// Processes the input equation and returns the amount of possible configurations to complete the equation.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="withConcat">Allow using the concatenation operator.</param>
+    /// <returns></returns>
+    public static (long Result, int PossibleCount) ProcessEquation(string input, bool withConcat = false)
+    {
+        // Grab the output and the input values:
+        var parts = input.Split(':');
+
+        long result = long.Parse(parts[0]);
+        int[] inputs = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        // Prepare all variations of the inputs.
+        var configs = OperationPermutations(inputs.Length - 1, new List<char>(), withConcat);
+
+        var possibleCount = 0;
+
+        foreach (var config in configs)
+        {
+            long configResult = inputs[0];
+
+            for (var i = 0; i < config.Length; i++)
+            {
+                if (config[i] == '+')
+                {
+                    configResult += inputs[i + 1];
+                }
+                else if (config[i] == '*')
+                {
+                    configResult *= inputs[i + 1];
+                }
+                else if (config[i] == '|' && withConcat)
+                {
+                    // Concatenate numbers.
+                    var temp = "" + configResult + inputs[i+1];
+                    configResult = long.Parse(temp);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Unexpected operator '{config[i]}' at position {i}");
+                }
+
+                if (configResult > result)
+                {
+                    // Result has already gone over - break out.
+                    break;
+                }
+            }
+
+            if (result == configResult)
+            {
+                ++possibleCount;
+            }
+        }
+
+        return (result, possibleCount);
+    }
+
+    /// <summary>
+    /// Returns all possible permutations of plus/multiply operations of the given length.
+    /// </summary>
+    /// <param name="length"></param>
+    /// <param name="buffer"></param>
+    /// <param name="withConcat"></param>
+    /// <returns></returns>
+    public static List<char[]> OperationPermutations(int length, IEnumerable<char> buffer, bool withConcat = false)
+    {
+        // If we've reached the full length, return the array.
+        if (buffer.Count() == length)
+        {
+            return new List<char[]> { buffer.ToArray() };
+        }
+
+        // Otherwise, return two variants of the buffer:
+        var output = new List<char[]>();
+        output.AddRange(OperationPermutations(length, buffer.Append('*'), withConcat));
+        output.AddRange(OperationPermutations(length, buffer.Append('+'), withConcat));
+        if (withConcat)
+        {
+            output.AddRange(OperationPermutations(length, buffer.Append('|'), withConcat));
+        }
+        return output;
+    }
+}
