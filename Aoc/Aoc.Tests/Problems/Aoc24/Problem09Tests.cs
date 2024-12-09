dfa58d71afa93683b9c24bea249e677da4a54889
Joni Saarelainen <2323657+Jonzki@users.noreply.github.com>
2024-12-09 08:10:08 +0200

    Solve 2024.09 part 1

---
+++ Aoc/Aoc.Tests/Problems/Aoc24/Problem09Tests.cs
@@ -0,0 +1,34 @@
+﻿using Aoc.Problems.Aoc24;
+
+namespace Aoc.Tests.Problems.Aoc24
+{
+    [TestClass]
+    public class Problem09Tests : ProblemTests<Problem09>
+    {
+        public const string SmallInput = "2333133121414131402";
+
+        public const string MiniInput = "12345";
+
+        [TestMethod]
+        public void SolvePart1()
+        {
+            RunPart1(1928L, SmallInput);
+        }
+
+        [TestMethod]
+        public void SolvePart2()
+        {
+            RunPart2(2858L, SmallInput);
+        }
+
+        [TestMethod]
+        public void ParseInput_ProducesCorrectDisk()
+        {
+            var disk = Problem09.ParseInput(MiniInput);
+
+            var diskString = Problem09.DiskToString(disk);
+
+            diskString.Should().Be("0..111....22222");
+        }
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc24/Problem09.cs
@@ -0,0 +1,151 @@
+﻿namespace Aoc.Problems.Aoc24;
+
+/// <summary>
+/// https://adventofcode.com/2024/day/9
+/// </summary>
+public class Problem09 : IProblem
+{
+    public object Solve1(string input)
+    {
+        var disk = ParseInput(input);
+        Console.WriteLine($"Disk length at start: {disk.Length}");
+
+        CompressPart1(disk);
+
+        var checksum = Checksum(disk);
+
+        return checksum;
+    }
+
+    public object Solve2(string input)
+    {
+        var disk = ParseInput(input);
+        Console.WriteLine($"Disk length at start: {disk.Length}");
+
+        CompressPart2(disk);
+
+        var checksum = Checksum(disk);
+
+        return checksum;
+    }
+
+    /// <summary>
+    /// Produces a "disk" array from the input string.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static int[] ParseInput(string input)
+    {
+        var output = new List<int>();
+
+        int id = 0;
+
+        // Input alternates between indicating
+        // the length of a file and the length of free space.
+        for (var i = 0; i < input.Length; ++i)
+        {
+            var num = input[i] - '0';
+
+            // Even = file, odd = empty space.
+            if (i % 2 == 0)
+            {
+                for (var n = 0; n < num; ++n)
+                {
+                    output.Add(id);
+                }
+            }
+            else
+            {
+                // When encountering empty space, increase the ID.
+                if (output.Count > 0 && output.Last() >= 0)
+                {
+                    id++;
+                }
+                // -1 = empty.
+                for (var n = 0; n < num; ++n)
+                {
+                    output.Add(-1);
+                }
+            }
+        }
+
+        return output.ToArray();
+    }
+
+    /// <summary>
+    /// Outputs a string format of the input Disk.
+    /// Useful for both tests and visualization.
+    /// </summary>
+    /// <param name="disk"></param>
+    /// <returns></returns>
+    public static string DiskToString(int[] disk)
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < disk.Length; ++i)
+        {
+            if (disk[i] >= 0)
+            {
+                sb.Append(disk[i]);
+            }
+            else
+            {
+                sb.Append('.');
+            }
+        }
+
+        return sb.ToString();
+    }
+
+    /// <summary>
+    /// Compresses the input Disk according to Part 1 rules.
+    /// </summary>
+    /// <param name="disk"></param>
+    public static void CompressPart1(int[] disk)
+    {
+        // Scan from rear and front to relocate blocks.
+        int front = 0;
+        int rear = disk.Length - 1;
+
+        while (front < rear)
+        {
+            // Scan the front index until we find an empty space.
+            for (; front < disk.Length; ++front)
+            {
+                if (disk[front] == EmptyBlock) { break; }
+            }
+            // Scan the rear index (backwards) until we find a block.
+            for (; rear >= 0; --rear)
+            {
+                if (disk[rear] >= 0) { break; }
+            }
+
+            if (rear > front)
+            {
+                // Move the block from rear to front.
+                disk[front] = disk[rear];
+                disk[rear] = EmptyBlock;
+            }
+        }
+    }
+
+    public static void CompressPart2(int[] disk)
+    {
+    }
+
+    public static long Checksum(int[] disk)
+    {
+        long output = 0;
+
+        for (var i = 0; i < disk.Length; ++i)
+        {
+            if (disk[i] != EmptyBlock)
+            {
+                output += i * disk[i];
+            }
+        }
+
+        return output;
+    }
+
+    private const int EmptyBlock = -1;
+}
