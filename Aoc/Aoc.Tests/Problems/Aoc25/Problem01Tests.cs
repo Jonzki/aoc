937e7dcd3adfc7b4ff7ce5b49f8e789d241f8782
Joni Saarelainen <joni@joni>
2025-12-01 18:24:29 +0200

    Solve 2025.01

--- Aoc/Aoc.Tests/Problems/Aoc25/Problem01Tests.cs
+++ Aoc/Aoc.Tests/Problems/Aoc25/Problem01Tests.cs
@@ -1,4 +1,5 @@
 ﻿using Aoc.Problems.Aoc25;
+using static Aoc.Problems.Aoc25.Problem01;
 
 namespace Aoc.Tests.Problems.Aoc25;
 
@@ -27,6 +28,98 @@ public class Problem01Tests : ProblemTests<Problem01>
     [TestMethod]
     public void SolvePart2()
     {
-        Assert.Inconclusive("TODO");
+        const string smallInput = """
+                                  L68
+                                  L30
+                                  R48
+                                  L5
+                                  R60
+                                  L55
+                                  L1
+                                  L99
+                                  R14
+                                  L82
+                                  """;
+
+        RunPart2(6, smallInput);
+    }
+
+    [TestMethod]
+    public void DialRotate_Handles_Negative()
+    {
+        var dial = new Problem01.Dial(10);
+
+        var clicks = dial.Rotate(-20);
+
+        dial.Position.Should().Be(90);
+        clicks.Should().Be(1);
+    }
+
+    [TestMethod]
+    public void DialRotate_Handles_Positive()
+    {
+        var dial = new Problem01.Dial(90);
+
+        var clicks = dial.Rotate(20);
+
+        dial.Position.Should().Be(10);
+        clicks.Should().Be(1);
+    }
+
+    [TestMethod]
+    public void DialRotate_Handles_MultiRotate()
+    {
+        var dial = new Problem01.Dial(50);
+
+        var clicks = dial.Rotate(1000);
+
+        dial.Position.Should().Be(50);
+        clicks.Should().Be(10);
+
+        clicks = dial.Rotate(-1000);
+
+        dial.Position.Should().Be(50);
+        clicks.Should().Be(10);
+    }
+
+    [TestMethod]
+    public void DialRotate_Handles_FromZero()
+    {
+        var dial = new Problem01.Dial(0);
+
+        // Rotate off from zero.
+        var clicks = dial.Rotate(10);
+        dial.Position.Should().Be(10);
+        clicks.Should().Be(0, "no click if started from zero");
+
+        // Rotate back to zero.
+        clicks = dial.Rotate(-10);
+        dial.Position.Should().Be(0);
+        clicks.Should().Be(1, "click because we reached zero");
+
+        // Rotate back to zero.
+        clicks = dial.Rotate(-10);
+        dial.Position.Should().Be(90);
+        clicks.Should().Be(0, "started from zero, did not roll over");
+
+        // Rotate back to zero.
+        clicks = dial.Rotate(-100);
+        dial.Position.Should().Be(90);
+        clicks.Should().Be(1, "started from 90, rolled over (left)");
+
+        // Full rotation from zero.
+        clicks = dial.Rotate(10);
+        dial.Position.Should().Be(0);
+        clicks.Should().Be(1, "click because we arrived back at zero from left");
+
+        // Full rotation from zero.
+        clicks = dial.Rotate(100);
+        dial.Position.Should().Be(0);
+        clicks.Should().Be(1, "click because we arrived back at zero");
+
+        // Full rotation from zero.
+        clicks = dial.Rotate(-200);
+        dial.Position.Should().Be(0);
+        clicks.Should().Be(2, "click because we made two full rotations");
     }
 }
--- Aoc/Aoc/Problems/Aoc25/Problem01.cs
+++ Aoc/Aoc/Problems/Aoc25/Problem01.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.Design;
 
 namespace Aoc.Problems.Aoc25;
 
@@ -10,11 +6,103 @@ public class Problem01 : IProblem
 {
     public object Solve1(string input)
     {
-        throw new NotImplementedException();
+        var actions = ParseDialActions(input);
+
+        var dial = new Dial();
+
+        // For part 1: The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+        int zeroCount = 0;
+
+        foreach (var action in actions)
+        {
+            dial.Rotate(action);
+            if (dial.Position == 0)
+            {
+                ++zeroCount;
+            }
+        }
+
+        return zeroCount;
     }
 
     public object Solve2(string input)
     {
-        throw new NotImplementedException();
+        var actions = ParseDialActions(input);
+
+        var dial = new Dial();
+
+        // For part 2: The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+        int clicks = 0;
+
+        foreach (var action in actions)
+        {
+            clicks += dial.Rotate(action);
+        }
+
+        return clicks;
+    }
+
+    public class Dial(int position = 50)
+    {
+        public int Position { get; private set; } = position;
+
+        public const int Steps = 100;
+
+        /// <summary>
+        /// Rotates the Dial by input amount. Returns the amount of zeroes visited (clicks for part 2).
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public int Rotate(int amount)
+        {
+            if (amount == 0)
+            {
+                return 0;
+            }
+
+            int clicks = 0;
+            int sign = Math.Sign(amount);
+
+            for (var i = 0; i < Math.Abs(amount); ++i)
+            {
+                Position += sign;
+
+                if (Position < 0)
+                {
+                    Position += Steps;
+                }
+                if (Position >= Steps)
+                {
+                    Position -= Steps;
+                }
+
+                if (Position == 0)
+                {
+                    ++clicks;
+                }
+            }
+
+            return clicks;
+        }
+    }
+
+    private List<int> ParseDialActions(string input)
+    {
+        var actions = input.SplitLines().Select(x =>
+        {
+            if (x.StartsWith('L'))
+            {
+                return -int.Parse(x.Substring(1));
+            }
+
+            if (x.StartsWith('R'))
+            {
+                return int.Parse(x.Substring(1));
+            }
+
+            throw new ArgumentException($"Unexpected format for input line: '{x}'");
+        }).ToList();
+
+        return actions;
     }
 }
