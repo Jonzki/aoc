b89760b18f9c0c53997b11581da6bf5fdde34d16
Joni Saarelainen <joni@joni>
2025-12-02 17:22:15 +0200

    Solve 2025.02

---
+++ Aoc/Aoc.Tests/Problems/Aoc25/Problem02Tests.cs
@@ -0,0 +1,73 @@
+﻿using Aoc.Problems.Aoc25;
+
+namespace Aoc.Tests.Problems.Aoc25;
+
+[TestClass]
+public class Problem02Tests : ProblemTests<Problem02>
+{
+    [TestMethod]
+    public void SolvePart1()
+    {
+        const string smallInput = """
+                                  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+                                  """;
+
+        RunPart1(1227775554L, smallInput);
+    }
+
+    [TestMethod]
+    public void SolvePart2()
+    {
+        const string smallInput = """
+                                  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+                                  """;
+
+        RunPart2(4174379265L, smallInput);
+    }
+
+    [DataTestMethod]
+    [DataRow(11L, true, "Seq: 1")]
+    [DataRow(12L, false, "No repeats")]
+    [DataRow(123123L, true, "Seq: 123")]
+    [DataRow(1231231L, false, "Partial seq: 123")]
+    [DataRow(123123123L, false, "Seq: 123 but should only be found twice")]
+    public void IsInvalidId_WorksFor_BasicCases_Part1(long input, bool correctIsInvalid, string reason)
+    {
+        var isInvalid = Problem02.IsInvalidId(input);
+
+        isInvalid.Should().Be(correctIsInvalid, reason);
+    }
+
+    [TestMethod]
+    public void IdRange_FindInvalidIds_Works_Small()
+    {
+        var range = Problem02.IdRange.Parse("11-22");
+        var invalidIds = range.FindInvalidIds();
+
+        invalidIds.Should().HaveCount(2);
+        invalidIds.Should().Contain(11L);
+        invalidIds.Should().Contain(22L);
+    }
+
+    [TestMethod]
+    public void IdRange_FindInvalidIds_Works_Large()
+    {
+        var range = Problem02.IdRange.Parse("1188511880-1188511890");
+        var invalidIds = range.FindInvalidIds();
+
+        invalidIds.Should().HaveCount(1);
+        invalidIds.Should().Contain(1188511885L);
+    }
+
+    [DataTestMethod]
+    [DataRow(11L, true, "Seq: 1")]
+    [DataRow(12L, false, "No repeats")]
+    [DataRow(123123L, true, "Seq: 123")]
+    [DataRow(123123123L, true, "Seq: 123, 3 times")]
+    public void IsInvalidId_WorksFor_BasicCases_Part2(long input, bool correctIsInvalid, string reason)
+    {
+        var isInvalid = Problem02.IsInvalidId(input, mode: 2);
+
+        isInvalid.Should().Be(correctIsInvalid, reason);
+    }
+}
---
+++ Aoc/Aoc/Problems/Aoc25/Problem02.cs
@@ -0,0 +1,127 @@
+﻿namespace Aoc.Problems.Aoc25;
+
+public class Problem02 : IProblem
+{
+    public object Solve1(string input)
+    {
+        var ranges = ParseRanges(input);
+
+        var invalidIds = ranges.SelectMany(r => r.FindInvalidIds(mode: 1));
+
+        return invalidIds.Sum();
+    }
+
+    public object Solve2(string input)
+    {
+        var ranges = ParseRanges(input);
+
+        var invalidIds = ranges.SelectMany(r => r.FindInvalidIds(mode: 2));
+
+        return invalidIds.Sum();
+    }
+
+    internal static List<IdRange> ParseRanges(string input)
+    {
+        var ranges = input.Split(',').Select(IdRange.Parse).ToList();
+
+        return ranges;
+    }
+
+    internal static bool IsInvalidId(long id, int mode = 1)
+    {
+        // Always parse the output.
+        var str = id.ToString();
+
+        // Pick the correct sequence comparer.
+        Func<string, string, bool> sequenceCompare = mode switch
+        {
+            1 => SequenceCompare1,
+            2 => SequenceCompare2,
+            _ => throw new ArgumentOutOfRangeException($"Unsupported mode: {mode}.")
+        };
+
+        // Check if the ID consists of repeating sequences.
+        for (int l = 1; l <= str.Length / 2; ++l)
+        {
+            // If the ID length is not evenly divisible by sequence length, it cannot be a repeating sequence.
+            if (str.Length % l != 0)
+            {
+                continue;
+            }
+
+            // Grab a substring from the start and repeat this however many times our input is.
+            var seq = str.Substring(0, l);
+
+            if (sequenceCompare(str, seq))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Part 1: check if the ID contains the test sequence exactly twice.
+    internal static bool SequenceCompare1(string idStr, string seq)
+    {
+        return idStr == seq + seq;
+    }
+
+    // Part 2: check if the ID contains the test sequence AT LEAST two times.
+    internal static bool SequenceCompare2(string idStr, string seq)
+    {
+        // ID length must be at least the length of two test sequences.
+        if (idStr.Length < seq.Length * 2)
+        {
+            return false;
+        }
+
+        // Removing all test sequences must result in an empty string.
+        return idStr.Replace(seq, string.Empty).Length == 0;
+    }
+
+    public class IdRange(long start, long end)
+    {
+        public long Start { get; init; } = start;
+        public long End { get; init; } = end;
+
+        public static IdRange Parse(string input)
+        {
+            var split = input.Split('-');
+            if (split.Length != 2)
+            {
+                throw new ArgumentException($"Invalid range: '{input}'");
+            }
+
+            if (!long.TryParse(split[0], out var start))
+            {
+                throw new ArgumentException($"Invalid start value: '{split[0]}'");
+            }
+            if (!long.TryParse(split[1], out var end))
+            {
+                throw new ArgumentException($"Invalid end value: '{split[1]}'");
+            }
+
+            return new IdRange(start, end);
+        }
+
+        /// <summary>
+        /// Finds invalid IDs in the range.
+        /// </summary>
+        /// <param name="mode">Controls whether the comparison is made with logic for part 1 or 2.</param>
+        /// <returns></returns>
+        public List<long> FindInvalidIds(int mode = 1)
+        {
+            List<long> output = new();
+
+            for (var id = Start; id <= End; ++id)
+            {
+                if (IsInvalidId(id, mode))
+                {
+                    output.Add(id);
+                }
+            }
+
+            return output;
+        }
+    }
+}
